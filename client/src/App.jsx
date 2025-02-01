a76f9e5856d016884090d131fc4c6a885b7bc0bb
MaheshND-321 <maheshndevraj@gmail.com>
2025-02-01 08:36:15 +0530

    added the page navigation in header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
-  return <div className="text-3xl text-red-700">app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 to-pink-500 rounded-lg text-white">
+          Mahi's
+        </span>
+        Blogs
+      </Link>
+      <form action="">
+        <TextInput
+          type="text"
+          placeholder="search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray">
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to="/signin">
+          <Button gradientDuoTone="purpleToBlue">Sign IN</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={Link} to="/">
+          Home
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={Link} to="/about">
+          About
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={Link} to="/projects">
+          Sign In
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
