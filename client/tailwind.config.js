348ba10c657c57d10e43910aeaccc2349a22b9b2
MaheshND-321 <maheshndevraj@gmail.com>
2025-02-24 23:55:01 +0530

    added the blogs listing with tables

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,10 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
   ],
+  darkMode: 'class', 
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
+    require("flowbite"),
+    require("tailwind-scrollbar"),
   ],
 }
