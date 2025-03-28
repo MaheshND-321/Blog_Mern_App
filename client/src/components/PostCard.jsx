b56b07ae19ffb5e40e3581560a9c64208ea7fa1c
MaheshND-321 <maheshndevraj@gmail.com>
2025-03-28 23:43:57 +0530

    added the postcards styling code under blog page

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PostCard = ({ post }) => {
   return (
-    <div className="group relative">
+    <div className="group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all">
       <Link to={`/post/${post.slug}`}>
         <img
           src={post.img}
@@ -11,10 +11,10 @@ const PostCard = ({ post }) => {
         />
       </Link>
       <div className="p-3 flex flex-col gap-2">
-        <p className="text-lg font-semibold ">{post.title}</p>
+        <p className="text-lg font-semibold line-clamp-2">{post.title}</p>
         <span className="italic text-sm">{post.category}</span>
         <Link
-          className="z-10 group-hover:bottom-0 absolute"
+          className="z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2"
           to={`/post/${post.slug}`}
         >
           Read article
